436903b1dff9bd91793f6592344aaf31d2e53d81
Max Jung <max@gourmetjs.com>
2019-02-18 12:41:47 -0800

    Add knexfile.js and `Log out` button.

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const args = require("@gourmet/server-args")();
+
+let knexConfig;
+
+if (args.stage === "local") {
+  knexConfig = {
+    client: "sqlite3",
+    connection: {
+      filename: `${__dirname}/.news-ssr.sqlite3`
+    }
+  };
+} else {
+  if (!process.env.PG_CONNECTION_STRING)
+    throw Error("PostgreSQL connection string must be given via an environment variable PG_CONNECTION_STRING");
+
+  knexConfig = {
+    client: "pg",
+    connection: process.env.PG_CONNECTION_STRING
+  };
+}
+
+module.exports = knexConfig;
--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import i80, {ActiveRoute} from "@gourmet/react-i80";
+import * as httpApi from "../utils/httpApi";
 import NewsView from "./NewsView";
 import SavedView from "./SavedView";
 
@@ -8,10 +9,25 @@ i80([
   ["/saved", SavedView]
 ]);
 
-export default function MainPage() {
+export default function MainPage({user}) {
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className="container" style={{padding: "2em 0"}}>
+      <div className="border-bottom mb-3 pb-2 text-right">
+        Hello {user.name}!
+        <button
+          type="button"
+          className="btn btn-outline-secondary btn-sm ml-3"
+          onClick={() => {
+            httpApi.post("/api/logout").then(() => {
+              window.location = "/login";
+            }).catch(err => {
+              console.error(err);
+            });
+          }}
+        >
+          Log out
+        </button>
+      </div>
       <ActiveRoute/>
     </div>
   );
