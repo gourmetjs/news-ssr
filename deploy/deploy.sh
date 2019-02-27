6c0574f53c79c5cceb064a36f5f690ef58850169
Max Jung <max@gourmetjs.com>
2019-02-26 21:25:12 -0800

    Add deployment scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,6 +9,6 @@ sudo rm -fr /var/app/news-ssr/*   # delete the current deployment
 
 # copy newly built files, and install dependencies in production mode
 sudo -u nodejs cp -R *.js *.json lib .gourmet /var/app/news-ssr/
-sudo -u nodejs -H /var/app/news-ssr npm install --production
+sudo -u nodejs npm install --prefix /var/app/news-ssr --production
 
 sudo systemctl start news-ssr     # restart the server
